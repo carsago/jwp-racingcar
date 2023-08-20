23155f6e1fffab708ec218359de5eb28361a40dd
hyunseo <ohseo8813@gmail.com>
2023-08-20 18:52:52 +0900

    깨지는거 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -13,6 +13,7 @@ public class InputView {
 
     public String readCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        System.out.println("hi");
         return scanner.nextLine();
     }
 
